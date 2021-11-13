82332852dfd5c3312daf20e26ca4392929c26119
jomit626 <jomit626@gmail.com>
2021-11-13 09:36:44 +0800

    tmp: nn in mixer

--- paqFe/Mixer.hpp
+++ paqFe/Mixer.hpp
@@ -9,10 +9,42 @@ class Mixer : Models... {
 
   static constexpr int n = sizeof...(Models);
 
-  Weight W[n][256];
+  //Weight W1[n][256][n];
+  Weight W[n];
   Prob X[n];
+  //Prob M[n];
   Context Ctx[n];
-  
+
+  Prob prev_prob = ProbEven;
+public:
+
+  Mixer() {}
+
+  Mixer(Models&&... models) 
+    : Models(models)...{
+    // getContext();
+  }
+
+  void init() {
+    
+  }
+
+  Prob predict(uint8_t bit) {
+    train(bit);
+
+    predict<Models...>(bit, X, Ctx);
+    Prob p = dot(X, W);
+
+    return p;
+  }
+
+  Prob predict_batch(uint8_t bit) {
+    // TODO: batch mode
+    return 0;
+  }
+
+private:
+
   template<typename M, typename ... Rest>
   void predict(uint8_t bit, Prob *pp, Context* ctx) {
     *pp = M::predict(bit);
@@ -20,6 +52,15 @@ class Mixer : Models... {
 
     if constexpr (sizeof...(Rest)) {
       predict<Rest...>(bit, ++pp, ++ctx);
+    }
+  }
+
+  template<typename M, typename ... Rest>
+  void getContext(Context* ctx) {
+    *ctx = M::getContext();
+
+    if constexpr (sizeof...(Rest)) {
+      getContext<Rest...>(++ctx);
     }
   }
 
@@ -32,31 +73,14 @@ class Mixer : Models... {
     return s;
   }
 
-public:
-
-  Mixer() {}
-
-  Mixer(Models&&... models) 
-    : Models(models)...{
-
-  }
-
-  void init() {
+  void train(uint8_t bit) {
+    uint32_t loss = (bit << 12) - prev_prob;
+    uint32_t lr = 1;
     
-  }
-
-  Prob predict(uint8_t bit) {
-    predict<Models...>(bit, X, Ctx);
-    int sum = 0;
     for(int i=0;i<n;i++)
-      sum += X[i];
-    return sum;
+      W[i] = W[i] + X[i]*loss*lr;
   }
 
-  Prob predict_batch(uint8_t bit) {
-    // TODO: batch mode
-    return 0;
-  }
 };
 
 }
--- paqFe/types.hpp
+++ paqFe/types.hpp
@@ -21,4 +21,23 @@ constexpr bool isPow2(const T& t) {
   return (t > 0 && (((t & (t - 1)) == 0)));
 }
 
+static constexpr int max(int a, int b) {
+  if(a > b)
+    return a;
+  else
+    return b;
+}
+/*
+template<int z, int x>
+class FixedPoint {
+  uint32_t x;
+  FixedPoint(uint32_t x) : x(x) {};
+
+  template<int z1, int x1>
+  FixedPoint<max(z, x1), max(z, x2)> operatro+(const FixedPoint<z1,x1>& other) {
+    return FixedPoint<std::max(z, x1), std::max(z, x2)> (x + other);
+  }
+};
+*/
+
 }
