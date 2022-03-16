24753ed6e46eb49c9825b6f0d5385f551281f4f9
jomit626 <jomit626@gmail.com>
2022-03-16 22:55:31 +0800

    impl: batched mixer

--- include/paqFe/Mixer.hpp
+++ include/paqFe/Mixer.hpp
@@ -14,18 +14,21 @@ protected:
   int32_t X[nFeature];
 
   Weight W1[nHidden];
+  Weight dW1[nHidden];
   int32_t X1[nHidden];
 
   Prob prev_prob = ProbEven;
   Context prev_ctx[nHidden];
+  int counter = 0;
+  static constexpr int BatchSize = 1024;
 public:
 
   Mixer() {
-    memset(W, 0x00, sizeof(W));
+    memset(dW1, 0x00, sizeof(dW1));
     Weight w = (1 << 16) / nFeature;
     Weight w1 = (1 << 16) / nHidden;
     for(int k=0;k<nHidden;k++) {
-      W1[k] = w;
+      W1[k] = w1;
       prev_ctx[k] = 0;
       X1[k] = 0;
       for(int j=0;j<256;j++)
@@ -55,7 +58,13 @@ public:
     for(int k=0;k<nHidden;k++) {
       train(W[k][prev_ctx[k]], X, nFeature, LUT.squash(X1[k]), bit, 7);
     }
-    train(W1, X1, nHidden, prev_prob, bit, 3);
+    accLoss(X1, nHidden, prev_prob, bit, 3, dW1);
+    if(counter % BatchSize == BatchSize - 1) {
+      counter = 0;
+      vecAdd(W1, dW1, nHidden);
+      memset(dW1,0,sizeof(dW1));
+    }
+    counter ++;
   }
 
   void update(uint8_t bit, Prob p) {
@@ -71,6 +80,26 @@ private:
     }
 
     return s;
+  }
+
+  void accLoss(int32_t *x, size_t len, Prob y, uint8_t bit, int lr, Weight *dws) {
+    int loss = ((bit << 12) - y) * lr;
+
+    for(int i=0;i<len;i++) {
+      Weight dw = dws[i] + ((((x[i] * loss * 2) >> 16U) + 1) >> 1U);
+      if( dw < -32768 ) {
+        dw = -32768;
+      } else if( dw > 32767 ) {
+        dw = 32767;
+      }
+      dws[i] = dw;
+    }
+  }
+
+  void vecAdd(int32_t* a, int32_t* b, size_t n){
+    for(int i=0;i<n;i++) {
+      a[i] += b[i];
+    }
   }
 
   void train(Weight *w, int32_t *x, size_t len, Prob y, uint8_t bit, int lr) {
