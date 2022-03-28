877af42abb25b2f91ea3784ed3d0600c5452bc4f
jomit626 <jomit626@gmail.com>
2022-03-28 08:55:39 +0800

    misc: project clean up

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # paqFe
 
-`paqFe` is a header-only C++ compression library using a PAQ-varient algorithm adapted for text compression and FPGA implementations. It has its [RTL model]() written with `Chisel`.  
+`paqFe` is a header-only C++ compression library using a PAQ-varient algorithm adapted for text compression and FPGA implementations. It has its [RTL model](https://github.com/Jomit626/paqFe-RTL) written in `Chisel`.  
 
 ## Overviwe
 
@@ -37,6 +37,25 @@ int main() {
 
 |file|gzip|lpaq1|paq8px|paqFe|
 |--|--|--|--|--|
+| bib     | 31.36%  | 20.80%  | 17.64% | 21.75% |
+| book1   | 40.62%  | 26.34%  | 23.86% | 28.37% |
+| book2   | 33.74%  | 21.43%  | 18.68% | 23.12% |
+| geo     | 66.80%  | 49.82%  | 41.49% | 46.77% |
+| news    | 38.28%  | 25.77%  | 22.07% | 27.90% |
+| obj1    | 47.96%  | 43.68%  | 32.89% | 42.86% |
+| obj2    | 32.85%  | 24.51%  | 16.61% | 23.91% |
+| paper1  | 34.86%  | 26.17%  | 23.31% | 27.68% |
+| paper2  | 36.08%  | 26.00%  | 23.81% | 27.62% |
+| paper3  | 38.83%  | 29.29%  | 26.64% | 31.01% |
+| paper4  | 41.60%  | 33.25%  | 29.97% | 35.20% |
+| paper5  | 41.72%  | 34.65%  | 30.94% | 36.53% |
+| paper6  | 34.65%  | 26.95%  | 23.60% | 28.51% |
+| pic     | 10.20%  | 8.50%   | 4.84%  | 8.64%  |
+| progc   | 33.46%  | 26.25%  | 22.47% | 27.31% |
+| progl   | 22.55%  | 16.81%  | 13.33% | 17.33% |
+| progp   | 22.64%  | 17.02%  | 12.99% | 17.58% |
+| trans   | 20.12%  | 14.31%  | 11.71% | 14.63% |
+| enwik8  |         | 24.77%  |        | 24.74% |
 
 ## License
 
@@ -48,9 +67,7 @@ The `paqFe` C++ library is licensed under the GNU General Public License v3.0. S
 
 I do remeber I named it for some reason, but I forgot it.
 
-- 
-
 ## Links
 
-- [LPAQ1]()
-- [PAQ8PX]()
+- [LPAQ1](http://www.mattmahoney.net/dc/#lpaq)
+- [PAQ8PX](https://github.com/hxim/paq8px)
--- test/src/contextmap.cpp
+++ test/src/contextmap.cpp
@@ -6,19 +6,37 @@
 using namespace paqFe::internal;
 using namespace paqFe;
 
-constexpr size_t n = 2000;
+const char data[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacca";
+constexpr size_t n = sizeof(data);
+
 
 size_t PassthroughHash(uint64_t val) {
   return val & ((1 << 12) - 1);
 }
 
+void display_prob(Prob* pp, size_t cols, size_t rows, int j) {
+  for(int i=0;i<rows;i++) {
+    char c = i % 8 == 7 ? '\n' : ' ';
+    printf("%d,%c", pp[i * cols + j], c);
+  }
+}
+void bits(uint8_t x) {
+  for(int i=7;i>=0;i--) {
+    uint8_t bit = (x >> i) & 0x1;
+    if(bit)
+      printf("1 ");
+    else
+      printf("0 ");
+  }
+  putchar('\n');
+}
 int main() {
   ContextMap<PassthroughHash,12> a;
   ContextMap<PassthroughHash,12> b;
 
   constexpr int cols = a.nProb;
   constexpr int rows = n * 8;
-
+  bits('a');
   Prob* pp = new Prob[cols * rows];
   Context* pctx = new Context[cols * rows];
   Prob* pp_test = new Prob[cols * rows];
@@ -28,23 +46,25 @@ int main() {
   pctx[0] = pctx[1] = pctx[2] = pctx[3] = 0;
   uint64_t ctx = 0;
   for(int i=0;i<n;i++) {
-    uint8_t byte = i;
+    uint8_t byte = data[i];
     for(int j=7;j>=0;j--) {
       uint8_t bit = (byte >> j) & 0x1;
       int row = i*8 + 7 - j + 1;  // +1 for first row is assumed to be 2048
       a.predict(bit, ctx, &pp[row * cols]);
     }
-    ctx = ctx << 8 | byte;
+    ctx = (ctx << 8 | byte) & 0xFF;
   }
 
   ctx = 0;
   for(int i=0;i<n;i++) {
-    uint8_t byte = i;
+    uint8_t byte = data[i];
     int row = i*8;
     b.predict_byte(byte, ctx, &pp_test[row * cols], cols);
-    ctx = ctx << 8 | byte;
+    ctx = (ctx << 8 | byte) & 0xFF;
   }
-
+  display_prob(pp, cols, rows, 0);
+  bits('a');
+  /*
   for(int i=0;i<rows;i++) {
     for(int j=0;j<cols;j++) {
       printf("i:%d,j:%d,exp:%d,real:%d\n", i / 8, i %8,pp[i * cols + j],pp_test[i * cols + j]);
@@ -52,6 +72,6 @@ int main() {
         return 1;
       }
     }
-  }
+  }*/
   return 0;
 }
