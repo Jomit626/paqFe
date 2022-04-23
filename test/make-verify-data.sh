559fb8d3e35ce61c4c819c33afedc4d9c091c29c
jomit626 <jomit626@gmail.com>
2022-04-23 09:21:27 +0800

    fix: fix fixpoint overflow

--- test/make-verify-data.sh
+++ test/make-verify-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-PROJ_FOLDER="$(dirname "$0")/.."
+PROJ_FOLDER="$(realpath $(dirname "$0")/..)"
 DATA_FOLDER="$PROJ_FOLDER/test/data"
 VERIFY_DATA_FOLDER="$PROJ_FOLDER/verify/data"
 
@@ -9,3 +9,4 @@ mkdir -p $VERIFY_DATA_FOLDER
 
 cat $DATA_FOLDER/book1 | head -n 8 > $VERIFY_DATA_FOLDER/small
 cat $DATA_FOLDER/book2 | head -n 64 > $VERIFY_DATA_FOLDER/medium
+dd if=/dev/zero of=$VERIFY_DATA_FOLDER/zeros count=100 bs=1024 2>/dev/null
